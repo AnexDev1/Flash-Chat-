a431ab92e49b7eec2ceca3d7e62966ae6c2c0e8a
anwar <anwarnasir0970@gmail.com>
2023-06-10 00:19:27 +0300

    minor fix 2

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flashchat/components/rounded_button.dart';
 import 'package:flashchat/constants.dart';
 import 'package:flashchat/screens/chat_screen.dart';
-import 'package:flashchat/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
